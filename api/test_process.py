e125ebe42ad4039cd8f9e727a292e46da7223445
Javlon_mac <richk880@gmail.com>
2023-07-09 07:35:21 +0500

    14, 19, 29-34, 37, 39, 43 are changed in test_process.py

--- api/test_process.py
+++ api/test_process.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from typing import Dict, List
+from typing import Dict, List, Optional
 from pydantic import BaseModel
 from database.questionservice import get_questions_db, check_user_answer_db
 from database.statservice import get_user_position
@@ -11,12 +11,12 @@ test_bp = Blueprint('test_process', __name__)
 # Валидатор вопросов
 class Questions(BaseModel):
     timer: int
-    questions: List[Dict[int, Dict[str, List[str]]]]
+    questions: List[Dict]
 
 
 # Запрос на получение всех вопросов
 @test_bp.route('/get-questions/<string:level>', methods=['GET'])
-def get_user_questions(level: str) -> Questions:
+def get_user_questions(level: str) -> Questions.json:
     # Обращение к базе вопросов
     questions = get_questions_db(level)
 
@@ -26,20 +26,21 @@ def get_user_questions(level: str) -> Questions:
 
         # пройдемся по каждому вопросу
         for question in questions:
-            generated_question = {question.main_question: [question.answer_1,
-                                                           question.answer_2,
-                                                           question.answer_3,
-                                                           question.answer_4]}
-            result.append({question.id: generated_question})
+            generated_question = {'question_text': question.main_question,
+                                  'variants': [question.answer_1,
+                                               question.answer_2,
+                                               question.answer_3,
+                                               question.answer_4]}
+            result.append({'question_id': question.id, 'question': generated_question})
             timer = question.timer
 
-        return Questions(**{'timer': timer, 'questions': result})
+        return Questions(**{'timer': timer, 'questions': result}).json()
 
-    return Questions(timer=0, questions=[])
+    return Questions(timer=0, questions=[]).json()
 
 
 # Запрос на проверку ответа от пользователя
-@test_bp.route('/check-answer/<int:question_id>/<string:user_answer>', methods=['POST'])
+@test_bp.route('/check-answer/<int:question_id>/<int:user_answer>', methods=['POST'])
 def check_current_user_answer(question_id: int, user_answer: str) -> Dict[str, int]:
     result = check_user_answer_db(question_id, user_answer)
 
